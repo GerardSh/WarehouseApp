b64d04a23f4b39182961497d69cccf599c2341c8
Gerard <gerardshah@gmail.com>
2025-04-27 10:32:32 +0300

    Add BaseController with IsGuidValid method for GUID validation

---
+++ WarehouseApp/WarehouseApp.Web/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseApp.Web.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected bool IsGuidValid(string? id, ref Guid cinemaGuid)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+
+            bool isGuidValid = Guid.TryParse(id, out cinemaGuid);
+            if (!isGuidValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- WarehouseApp/WarehouseApp.Web/Controllers/HomeController.cs
+++ WarehouseApp/WarehouseApp.Web/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WarehouseApp.Web.Controllers;
 using WarehouseApp.Web.ViewModels;
 
 namespace WarehouseApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
