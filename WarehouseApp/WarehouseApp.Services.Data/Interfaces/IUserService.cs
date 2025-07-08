0fa5eacd712b762ce8fe2478da503dde4de8c7b6
Gerard <gerardshah@gmail.com>
2025-07-08 23:48:06 +0300

    Add IUserService and UserService for managing user accounts

--- WarehouseApp/WarehouseApp.Services.Data/Interfaces/IUserService.cs
+++ WarehouseApp/WarehouseApp.Services.Data/Interfaces/IUserService.cs
@@ -1,6 +1,17 @@
-﻿//namespace WarehouseApp.Services.Data.Interfaces
-//{
-//    public interface IUserService
-//    {
-//    }
-//}
+﻿using WarehouseApp.Web.ViewModels.Admin.UserManagement;
+
+namespace WarehouseApp.Services.Data.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<AllUsersViewModel>> GetAllUsersAsync();
+
+        Task<bool> UserExistsByIdAsync(Guid userId);
+
+        Task<bool> AssignUserToRoleAsync(Guid userId, string roleName);
+
+        Task<bool> RemoveUserRoleAsync(Guid userId, string roleName);
+
+        Task<bool> DeleteUserAsync(Guid userId);
+    }
+}
