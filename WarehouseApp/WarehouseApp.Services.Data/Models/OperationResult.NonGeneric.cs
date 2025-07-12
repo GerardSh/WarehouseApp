7ae07ac488d3e09fe6affb7d6920043ed17a610c
Gerard <gerardshah@gmail.com>
2025-07-12 23:15:19 +0300

    Refactor OperationResult to add non-generic class for simpler usage

---
+++ WarehouseApp/WarehouseApp.Services.Data/Models/OperationResult.NonGeneric.cs
@@ -0,0 +1,14 @@
+﻿namespace WarehouseApp.Services.Data.Models
+{
+    public class OperationResult
+    {
+        public bool Success { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        public static OperationResult Failure(string message) =>
+            new OperationResult { Success = false, ErrorMessage = message };
+
+        public static OperationResult Ok() =>
+            new OperationResult { Success = true, };
+    }
+}
--- WarehouseApp/WarehouseApp.Services.Data/Models/OperationResult.cs
+++ WarehouseApp/WarehouseApp.Services.Data/Models/OperationResult.cs
@@ -1,12 +1,10 @@
 ﻿namespace WarehouseApp.Services.Data.Models
 {
-    public class OperationResult<T>
+    public class OperationResult<T> : OperationResult
     {
-        public bool Success { get; set; }
-        public string? ErrorMessage { get; set; }
         public T? Data { get; set; }
 
-        public static OperationResult<T> Failure(string message) =>
+        public new static OperationResult<T> Failure(string message) =>
             new OperationResult<T> { Success = false, ErrorMessage = message };
 
         public static OperationResult<T> Ok(T? data = default) =>
